7e5976f255685d65b5244d6a559b34570d92392b
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-05-15 14:25:15 +0700

    Changed SPARTA 2019 to 2018

--- src/components/RegistrationPartials/Finished.vue
+++ src/components/RegistrationPartials/Finished.vue
@@ -2,7 +2,7 @@
     <v-form @submit.prevent="proceed" ref="form">
         <div class="pa-4" align="left">
             <v-img :src="require('../../assets/spartalogo.png')" width="80"></v-img>
-            <div class="display-1 font-weight-bold" style="margin-top: 24px">Selamat datang di SPARTA 2019!</div>
+            <div class="display-1 font-weight-bold" style="margin-top: 24px">Selamat datang di SPARTA 2018!</div>
             <p class="mt-3">
                 Perjalanan kalian masih panjang, jangan menyerah dan teruslah berkarya dan berkontribusi! Sisanya biar medinfo aja ya yg nulis lol.
             </p>
